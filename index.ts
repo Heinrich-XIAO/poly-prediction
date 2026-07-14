9189c640fcc526ecd44ba3a6325b8a83e4149329
Heinrich-XIAO <74563446+Heinrich-XIAO@users.noreply.github.com>
2026-07-14 17:16:52 +0800

    stuff

--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
-import { createPublicClient } from "@polymarket/client";
+import { createPublicClient, PriceHistoryInterval } from "@polymarket/client";
 
 const client = createPublicClient();
+
+const firstSlug = "bitcoin-up-or-down-june-19-12am-et";
 
 function createSlugGenerator(start: Date) {
   let current = new Date(start);
@@ -24,17 +26,50 @@ function createSlugGenerator(start: Date) {
     // Move back one hour for the next call
     current.setUTCHours(current.getUTCHours() - 1);
 
-    return slug;
+    return {
+      slug,
+      time: current.getTime()/1000
+    };
   };
 }
 
 const nextSlug = createSlugGenerator(new Date("2026-07-13T23:00:00-04:00"));
 
 const checkNextSlug = async () => {
-  const slug = nextSlug();
-  const res = await client.fetchMarket({
-    slug
-  })
-  console.log(res);
+  const {slug, time} = nextSlug();
+  try {
+    const res = await client.fetchMarket({
+      slug
+    });
+    console.log(res);
+    try {
+      if (!res.outcomes || !res.outcomes.yes || !res.outcomes.no || !res.outcomes.yes.tokenId || !res.outcomes.no.tokenId) {
+        console.error(`No 'yes' or 'no' outcome found for market with slug: ${slug}`);
+        return false;
+      }
+      const crypto_price_history = await (await fetch(`https://polymarket.com/api/crypto/price-history?symbol=BTC&eventStartTime=${encodeURIComponent(new Date(time*1000).toISOString().replace('.000', ''))}&variant=hourly&endDate=${encodeURIComponent(new Date(new Date(time*1000).getTime() + 60 * 60 * 1000).toISOString().replace(".000", ""))}`)).json();
+      console.log(crypto_price_history);
+
+      console.log(res.outcomes.yes.tokenId);
+      const yes_history = await client.fetchPriceHistory({
+        tokenId: res.outcomes.yes.tokenId,
+        interval: PriceHistoryInterval.ONE_HOUR,
+      });
+      console.log(yes_history);
+      const no_history = await client.fetchPriceHistory({
+        tokenId: res.outcomes.no.tokenId,
+        interval: PriceHistoryInterval.MAX,
+      });
+      console.log(no_history);
+    } catch (error) {
+      console.error(`Error fetching market history for slug: ${slug}`, error);
+      return false;
+    }
+  } catch (error) {
+    console.error(`Error fetching market for slug: ${slug}`, error);
+    return false;
+  }
+  return true;
 };
-checkNextSlug();
+// while (await checkNextSlug()) {}
+await checkNextSlug();
